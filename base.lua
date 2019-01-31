65b43ef2cfc2cf15fe5166d3a8576c17840e67a8
sh-zam <32485569+sh-zam@users.noreply.github.com>
2019-02-01 01:18:58 +0530

    minor bug fix
    Use absolute path in store

--- base.lua
+++ base.lua
@@ -21,6 +21,6 @@ end
 -- Add all directories in contentdb to the stores table
 for entry in fs.entries(db_path) do
   if fs.metadata(db_path .. entry).type == "directory" then
-    contentdb.stores[entry] = "content/" .. entry .. "/"
+    contentdb.stores[entry] = db_path .. entry .. "/"
   end
 end
